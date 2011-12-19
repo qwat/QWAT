9e540785e46c7fb43f5cb7c9b4c8d2f4877bd495
3nids <Denis,Rouzaud@gmail.com>
2011-12-19 17:24:51 +0100

    added comment to pipes_status

--- sql/pipes_status.sql
+++ sql/pipes_status.sql
@@ -1,22 +1,33 @@
-DROP TABLE "distribution"."pipes_status" CASCADE;
-BEGIN;
+/*
+	qWat - QGIS Water Module
+	
+	SQL file :: pipes_status table
+*/
+
+DROP TABLE IF EXISTS "distribution"."pipes_status" CASCADE;
 CREATE TABLE "distribution"."pipes_status" ( id SERIAL, CONSTRAINT "pipes_status_pk" PRIMARY KEY (id) );
 
-ALTER TABLE "distribution"."pipes_status" ADD COLUMN "strid" VARCHAR;
+/* Columns */
+ALTER TABLE "distribution"."pipes_status" ADD COLUMN "strid" VARCHAR; /* TB2 id in table */
 ALTER TABLE "distribution"."pipes_status" ADD COLUMN "status" VARCHAR;
 ALTER TABLE "distribution"."pipes_status" ADD COLUMN "active" BOOLEAN DEFAULT true;
 
+/* Constraints */
 ALTER TABLE distribution.pipes_status ADD CONSTRAINT status_name UNIQUE ("status");
 
+/* Comment */
+COMMENT ON TABLE distribution.pipes_status IS 'Status for pipes. Here is determined if the pipe is active or not. By nature, only one satus is active.';
 
+
+/* Predefined functions list */
+/* This has to be filled by user to fullfile language preferences */
 INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('E','en service',true);
 INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('EA','en attente',false);
 INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('F','fictif',false);
 INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('A','abandonnee',false);
 INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('D','desaffectee',false);
-
-
 /*
+Unused status from TB 2.
 INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('I','inconnu',true);
 INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('HS','hors service',true);
 INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('C','variante de calcul',true);
@@ -28,4 +39,3 @@ INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('PA','projet
 */
 
 
-COMMIT;
