192c0718fa04d2abe79a74eb3b461163bb98226b
Denis Rouzaud <Denis,Rouzaud@gmail.com>
2011-12-19 16:40:08 +0100

    Added to comments to printable_maps

--- sql/printable_maps.sql
+++ sql/printable_maps.sql
@@ -19,21 +19,30 @@ SELECT AddGeometryColumn('distribution', 'printable_maps', 'wkb_geometry', 21781
 /* primary key */
 ALTER TABLE distribution.printable_maps ADD CONSTRAINT print_pkey PRIMARY KEY (fid);
 
+/* Comment */
+COMMENT ON TABLE distribution.printable_maps IS 'This table is used for polygons for predefined printable maps. short_name would be used as label string, and long_mame would be used in the print composer.';
+
 
 /*----------------!!!---!!!----------------*/
 /* FUNCTION */
 DROP AGGREGATE IF EXISTS distribution.tsum(text);
 CREATE AGGREGATE distribution.tsum ( BASETYPE = text, SFUNC = textcat, STYPE = text, INITCOND = '' );
+COMMENT ON AGGREGATE distribution.tsum IS 'Used to concatenate text in a SELECT. See distribution.getMapName for an example.';
+
 
 CREATE OR REPLACE FUNCTION distribution.getMapName(integer) RETURNS text AS '
 	DECLARE
 		pipe_id ALIAS FOR $1;
 		result text;
 	BEGIN
-		SELECT left(distribution.tsum(printable_maps.short_name || '', ''),-2) INTO result FROM distribution.printable_maps, distribution.pipes WHERE pipes.fid = pipe_id AND ST_Intersects(pipes.wkb_geometry,printable_maps.wkb_geometry) IS TRUE;
+		SELECT left(distribution.tsum(printable_maps.short_name || '', ''),-2) INTO result
+			FROM distribution.printable_maps, distribution.pipes WHERE pipes.fid = pipe_id
+			 AND ST_Intersects(pipes.wkb_geometry,printable_maps.wkb_geometry) IS TRUE;
 		RETURN result;
 	END
 ' LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION distribution.getMapName(integer) IS 'Returns a string contaning all the short names of the polygons in table printable_maps which overlap the given pipe.';
+
 
 CREATE OR REPLACE FUNCTION distribution.fillMapNames() RETURNS boolean AS '
 	BEGIN
@@ -41,6 +50,8 @@ CREATE OR REPLACE FUNCTION distribution.fillMapNames() RETURNS boolean AS '
 		RETURN true;
 	END
 ' LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION distribution.fillMapNames() IS 'Populates the is_on_map field in pipes table with all the short names of poylgons in table printable_maps which overlap the given pipe.';
+
 
 
 
