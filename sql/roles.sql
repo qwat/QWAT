32d77eaf132424981ba89394bc703870ce259e13
3nids <denis.rouzaud@gmail.com>
2012-01-03 11:38:44 +0100

    SQL: completed roles

--- sql/roles.sql
+++ sql/roles.sql
@@ -3,22 +3,46 @@
 	
 	SQL file :: database roles
 */
+
+/* 
+Viewer role - can SELECT all the tables
+*/
 CREATE ROLE "viewer";
-CREATE ROLE "user" IN ROLE "viewer";
-CREATE ROLE "poweruser"  IN ROLE "user";
-CREATE ROLE "admin" SUPERUSER;
-
-
 ALTER DEFAULT PRIVILEGES IN SCHEMA distribution,public
     GRANT SELECT ON TABLES
     TO "viewer";
-      
+       
+/* 
+Simple user role - can update geometric objects except their geometries
+*/    
+CREATE ROLE "simpleuser" IN ROLE "viewer";
 ALTER DEFAULT PRIVILEGES IN SCHEMA distribution
     GRANT INSERT, UPDATE, DELETE ON TABLES
+    TO "simpleuser"; 
+REVOKE INSERT,DELETE ON distribution.pipes FROM simpleuser;
+REVOKE UPDATE (wkb_geometry) ON distribution.pipes FROM simpleuser;
+
+/* 
+Power user role - can add/update/delete any object
+*/ 
+CREATE ROLE "poweruser"  IN ROLE "viewer";
+ALTER DEFAULT PRIVILEGES IN SCHEMA distribution
+    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES
     TO "poweruser";    
+
+/* 
+Admin role
+*/ 
+CREATE ROLE "admin" SUPERUSER;
+
+
+
+
+
+      
+
     
-    
-GRANT U
+
     
 
 
