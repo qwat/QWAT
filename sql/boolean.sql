581a8de1d93fdb9ae38d1f4e2f6e6f759e0ba153
3nids <denis.rouzaud@gmail.com>
2012-01-04 16:23:14 +0100

    SQL: changed boolean table name

--- sql/boolean.sql
+++ sql/boolean.sql
@@ -1,23 +1,23 @@
 /*
 	qWat - QGIS Water Module
 	
-	SQL file :: boolean table for value relation in qgis
+	SQL file :: boolean tables for value relation in qgis
 */
 
 /*----------------!!!---!!!----------------*/
 BEGIN;
 
-DROP TABLE IF EXISTS distribution."boolean" CASCADE;
-CREATE TABLE distribution."boolean" ( id SERIAL, CONSTRAINT "boolean_pk" PRIMARY KEY (id) );
+DROP TABLE IF EXISTS distribution.visible CASCADE;
+CREATE TABLE distribution.visible ( id SERIAL, CONSTRAINT "visible_pk" PRIMARY KEY (id) );
 
-ALTER TABLE distribution."boolean" ADD COLUMN  value boolean;
-ALTER TABLE distribution."boolean" ADD COLUMN  name  varchar(50);
+ALTER TABLE distribution.visible ADD COLUMN  value boolean;
+ALTER TABLE distribution.visible ADD COLUMN  name  varchar(50);
 
-INSERT INTO distribution."boolean" (value,name) VALUES (NULL,'Auto');
-INSERT INTO distribution."boolean" (value,name) VALUES (True,'Oui');
-INSERT INTO distribution."boolean" (value,name) VALUES (False,'Non');
+INSERT INTO distribution.visible (value,name) VALUES (NULL,'Auto');
+INSERT INTO distribution.visible (value,name) VALUES (True,'Visible');
+INSERT INTO distribution.visible (value,name) VALUES (False,'Invisible');
 
-COMMENT ON TABLE distribution."boolean" IS 'Table for editing in qgis tri state boolean (NULL/True/False) by using value relation.';
+COMMENT ON TABLE distribution.visible IS 'Table for editing in qgis tri state boolean (NULL/True/False) by using value relation.';
 
 COMMIT;
 
