ace5552e2c7e7a377d6d6a6b6af17e128806e3b8
3nids <denis.rouzaud@gmail.com>
2012-01-05 15:10:38 +0100

    SQL: removed useless column from pipes_status (TB)

--- sql/pipes_status.sql
+++ sql/pipes_status.sql
@@ -9,8 +9,7 @@ DROP TABLE IF EXISTS "distribution"."pipes_status" CASCADE;
 CREATE TABLE "distribution"."pipes_status" ( id SERIAL, CONSTRAINT "pipes_status_pk" PRIMARY KEY (id) );
 
 /* Columns */
-ALTER TABLE "distribution"."pipes_status" ADD COLUMN "strid" VARCHAR; /* TB2 id in table */
-ALTER TABLE "distribution"."pipes_status" ADD COLUMN "status" VARCHAR;
+ALTER TABLE "distribution"."pipes_status" ADD COLUMN "status" VARCHAR(30);
 ALTER TABLE "distribution"."pipes_status" ADD COLUMN "active" BOOLEAN DEFAULT true;
 
 /* Constraints */
@@ -22,21 +21,21 @@ COMMENT ON TABLE distribution.pipes_status IS 'Status for pipes. Here is determi
 
 /* Predefined functions list */
 /* This has to be filled by user to fullfile language preferences */
-INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('E','en service',true);
-INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('EA','en attente',false);
-INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('F','fictif',false);
-INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('A','abandonnee',false);
-INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('D','desaffectee',false);
+INSERT INTO distribution.pipes_status (status,active) VALUES ('en service',true);
+INSERT INTO distribution.pipes_status (status,active) VALUES ('en attente',false);
+INSERT INTO distribution.pipes_status (status,active) VALUES ('fictif',false);
+INSERT INTO distribution.pipes_status (status,active) VALUES ('abandonnee',false);
+INSERT INTO distribution.pipes_status (status,active) VALUES ('desaffectee',false);
 /*
 Unused status from TB 2.
-INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('I','inconnu',true);
-INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('HS','hors service',true);
-INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('C','variante de calcul',true);
-INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('PDDE','prevu PDDE',true);
-INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('PR','projet',true);
-INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('P','provisoire',true);
-INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('CR','condamne/rempli',true);
-INSERT INTO distribution.pipes_status (strid,status,active) VALUES ('PA','projet approuve',true);
+INSERT INTO distribution.pipes_status (status,active) VALUES ('I','inconnu',true);
+INSERT INTO distribution.pipes_status (status,active) VALUES ('HS','hors service',true);
+INSERT INTO distribution.pipes_status (status,active) VALUES ('C','variante de calcul',true);
+INSERT INTO distribution.pipes_status (status,active) VALUES ('PDDE','prevu PDDE',true);
+INSERT INTO distribution.pipes_status (status,active) VALUES ('PR','projet',true);
+INSERT INTO distribution.pipes_status (status,active) VALUES ('P','provisoire',true);
+INSERT INTO distribution.pipes_status (status,active) VALUES ('CR','condamne/rempli',true);
+INSERT INTO distribution.pipes_status (status,active) VALUES ('PA','projet approuve',true);
 */
 
 COMMIT;
