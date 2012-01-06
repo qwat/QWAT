868041b117386058f2e7b83a16b01805cb37bfa5
3nids <denis.rouzaud@gmail.com>
2012-01-06 15:41:06 +0100

    SQL: precision table: capital letters, remove useless label pos fields

--- sql/precision.sql
+++ sql/precision.sql
@@ -10,9 +10,7 @@ DROP TABLE IF EXISTS distribution."precision" CASCADE;
 CREATE TABLE distribution."precision" (id serial NOT NULL);
 
 ALTER TABLE distribution."precision" ADD COLUMN  name       varchar(30);
-ALTER TABLE distribution."precision" ADD COLUMN  lbl_x double precision;
-ALTER TABLE distribution."precision" ADD COLUMN  lbl_y double precision;
-ALTER TABLE distribution."precision" ADD COLUMN  lbl_a double precision;
+
 
 /* ADD CONSTRAINTS */
 /* primary key */
@@ -23,10 +21,10 @@ ALTER TABLE distribution."precision" ADD CONSTRAINT precision_name UNIQUE (name)
 COMMENT ON TABLE distribution."precision" IS 'pressure "precision".';
 
 
-INSERT INTO distribution."precision" (name) VALUES ('imprécis');    /*TB: IP => 1 */
-INSERT INTO distribution."precision" (name) VALUES ('précis');      /*TB: P  => 2 */
-INSERT INTO distribution."precision" (name) VALUES ('localisé');    /*TB: L  => 3 */
-INSERT INTO distribution."precision" (name) VALUES ('digitalisé');  /*TB: D  => 4 */
-INSERT INTO distribution."precision" (name) VALUES ('inconnu');     /*TB: I  => 5 */
+INSERT INTO distribution."precision" (name) VALUES ('Imprécis');    /*TB: IP => 1 */
+INSERT INTO distribution."precision" (name) VALUES ('Précis');      /*TB: P  => 2 */
+INSERT INTO distribution."precision" (name) VALUES ('Localisé');    /*TB: L  => 3 */
+INSERT INTO distribution."precision" (name) VALUES ('Digitalisé');  /*TB: D  => 4 */
+INSERT INTO distribution."precision" (name) VALUES ('Inconnu');     /*TB: I  => 5 */
 
 COMMIT;
