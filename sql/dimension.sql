68ccf275dec68cf91c08f6acdcf1880c203d0029
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-01-13 13:06:09 +0100

    SQL: put back rules for dimension

--- sql/dimension.sql
+++ sql/dimension.sql
@@ -49,10 +49,7 @@ INSERT INTO geometry_columns (f_table_catalog, f_table_schema, f_table_name, f_g
 	VALUES  ('' , 'distribution', 'dimension_view', 'wkb_geometry', 2, 21781, 'LINESTRING');
 /* Comment */	
 COMMENT ON VIEW distribution.dimension_view IS 'Creates a Linestring so it can be viewed in QGIS as a curve. Later, this view should be remove and the new geometry transformed to CIRCULARSTRING.';
-
-
 /* insert,update,delete rules */
-/*
 CREATE OR REPLACE RULE dimension_update AS
 	ON UPDATE TO distribution.dimension_view DO INSTEAD
 		UPDATE distribution.dimension SET 
@@ -70,15 +67,10 @@ CREATE OR REPLACE RULE dimension_insert AS
 CREATE OR REPLACE RULE dimension_delete AS
 	ON DELETE TO distribution.dimension_view DO INSTEAD
 		DELETE FROM distribution.dimension WHERE id = OLD.id;
-*/
 /* Comments */	
-/*
 COMMENT ON RULE dimension_update IS 'Rule to forward changes for dimension_view to the table dimension.';
 COMMENT ON RULE dimension_insert IS 'Rule to forward insert of dimension_view to the table dimension.';
 COMMENT ON RULE dimension_delete IS 'Rule to forward deletion of dsimension_view to the table dimension.';
-*/
-/*
-INSERT INTO distribution.dimension (wkb_geometry) VALUES (ST_GeomFromText('MULTIPOINT(0 0,1 3,2 0)'));
-*/
+
 
 COMMIT;
