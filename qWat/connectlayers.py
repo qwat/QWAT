a8f59fa0dcba349c17357d939530f9f93b361fe2
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-01-18 11:45:06 +0100

    new method to connect layers

--- qWat/connectlayers.py
+++ qWat/connectlayers.py
@@ -9,7 +9,9 @@ qWat - qgis water module
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+from qgis.core import *
 from ui_connectLayers import Ui_connectLayers
+from itembrowser import itemBrowser
 
 try:
     _fromUtf8 = QString.fromUtf8
@@ -21,21 +23,46 @@ class connectLayers(QDialog, Ui_connectLayers ):
 	def __init__(self,iface):
 		self.iface = iface
 		QDialog.__init__(self)
-		# Set up the user interface from Designer.
 		self.setupUi(self)
-			
-			
+		QObject.connect(self , SIGNAL( "accepted()" ) , self.applySettings)
+		self.combos      = [ self.pipesCombo  , self.valvesCombo   ]
+		self.settingName = [     "pipes_layer",     "valves_layer" ]
+				
 	def showEvent(self, e):
-		proj = QgsProject.instance()
-		combos       = [                    self.pipesCombo                             , self.valvesCombo           ]
-		currentValue = [proj.readEntry("qWat", "pipes_layer", "")[0] , proj.readEntry("qWat", "valves_layer", "")[0] ]
-		
-		for combo in combos:
-			combo.reset()
+		self.proj   = QgsProject.instance()
+		self.layers = self.iface.mapCanvas().layers()
+	
+		for c in range(len(self.combos)):
+			combo = self.combos[c]
+			combo.clear()
 			combo.addItem(_fromUtf8(""))
-			l = 0
-			for layer in self.iface.mapCanvas().layers():
+			l = 1
+			for layer in self.layers:
 				combo.addItem(_fromUtf8(""))
 				combo.setItemText(l, layer.name())
-				if layer.id() == 
+				if layer.id() == self.proj.readEntry("qWat",self.settingName[c],"")[0]:
+					combo.setCurrentIndex(l)
 				l+=1
+				
+	def applySettings(self):
+		for c in range(len(self.combos)):
+			# TODO if pipelayer.type() != QgsMapLayer.VectorLayer or pipelayer.geometryType() != 1 and pipelayer.dataProvider().fieldNameIndex('id_parent') == -1:
+			self.proj.writeEntry("qWat", self.settingName[c] , self.layer(c).id() )
+		self.connect()
+		
+	def layer(self,c):
+		i = self.combos[c].currentIndex()
+		if i == 0: return emptyLayer()
+		else: return self.layers[i-1]
+		
+	def connect(self):
+		for setting in self.settingName:
+			layer = next( ( layer for layer in self.iface.mapCanvas().layers() if layer.id() == QgsProject.instance().readEntry("qWat", setting, "")[0] ), False )
+			if layer is not False:
+				self.itembrowser = itemBrowser( self.iface , layer , QApplication.translate("Browser", "qWat :: Pipes", "Window title", QApplication.UnicodeUTF8) )
+				
+				
+class emptyLayer():
+	def id(self):
+		return ''
+				
--- qWat/qwat.py
+++ qWat/qwat.py
@@ -15,7 +15,7 @@ from qgis.core import *
 # Import the code for the dialog
 from pipesearch import pipeSearch
 from connectlayers import connectLayers
-from itembrowser import itemBrowser
+
 
 # Initialize Qt resources from file resources.py
 import resources
@@ -32,53 +32,32 @@ class qWat ():
 		# Save reference to the QGIS interface
 		self.iface = iface
 		self.pipelayer = 0
-		self.connectLayers = connectLayers(iface)
+		self.connectLayersDlg = connectLayers(iface)
 
 	def initGui(self):
 		# CONNECTLAYERS
-		self.ConnectLayerAction = QAction(QIcon(":/plugins/qWat/icons/connect.png"), "connect layers", self.iface.mainWindow())
+		self.connectLayerAction = QAction(QIcon(":/plugins/qWat/icons/connect.png"), "connect layers", self.iface.mainWindow())
 		# connect the action to the run method
-		QObject.connect(self.ConnectLayerAction, SIGNAL("triggered()"), connectLayers.exec_())
+		QObject.connect(self.connectLayerAction, SIGNAL("triggered()"), self.connectLayersDlg.exec_)
 		# Add toolbar button and menu item
-		self.iface.addToolBarIcon(self.ConnectLayerAction)
-		self.iface.addPluginToMenu("&qWat", self.ConnectLayerAction)
+		self.iface.addToolBarIcon(self.connectLayerAction)
+		self.iface.addPluginToMenu("&qWat", self.connectLayerAction)
 		
 		# PIPESEARCH
-		self.PipeSearchAction = QAction(QIcon(":/plugins/qWat/icons/search.png"), "pipe search", self.iface.mainWindow())
-		QObject.connect(self.PipeSearchAction, SIGNAL("triggered()"), self.PipeSearchDlg)
+		self.pipeSearchAction = QAction(QIcon(":/plugins/qWat/icons/search.png"), "pipe search", self.iface.mainWindow())
+		QObject.connect(self.pipeSearchAction, SIGNAL("triggered()"), self.PipeSearchDlg)
 		# Add toolbar button and menu item
-		self.iface.addToolBarIcon(self.PipeSearchAction)
-		self.iface.addPluginToMenu("&qWat", self.PipeSearchAction)
+		self.iface.addToolBarIcon(self.pipeSearchAction)
+		self.iface.addPluginToMenu("&qWat", self.pipeSearchAction)
 				
 		
 	def unload(self):
 		print "TODO: qWat unload"
 		# TODO
 		# Remove the plugin menu item and icon
-		#self.iface.removePluginMenu("&SIGE",self.PipeSearchAction)
-		#self.iface.removeToolBarIcon(self.PipeSearchAction)
+		#self.iface.removePluginMenu("&qWat",self.pipeSearchAction)
+		#self.iface.removeToolBarIcon(self.pipeSearchAction)
 		
-	def disconnectPipeLayer(self):
-		self.pipelayer = 0
-		self.itembrowser.unload()
-		
-	def ConnectLayersDlg(self):
-		# create and show the dialog
-		layerlist = self.iface.mapCanvas().layers()
-		dlg = ConnectLayers(layerlist)
-		if dlg.exec_():
-			# TODO: check layers before starting connection! 
-			pipelayer = layerlist[dlg.pipesCombo.currentIndex()]
-			if pipelayer.type() != QgsMapLayer.VectorLayer or pipelayer.geometryType() != 1 and pipelayer.dataProvider().fieldNameIndex('id_parent') == -1:
-				self.pipelayer = 0;
-				QMessageBox.warning( dlg , "qWat", QApplication.translate("Connect layers", "The pipe layer has not a valid structure! Check again!", None, QApplication.UnicodeUTF8) )
-				return
-			else:
-				self.itembrowser = ItemBrowser( self.iface , pipelayer , QApplication.translate("Browser", "qWat :: Pipes", "Window title", QApplication.UnicodeUTF8) )
-				QObject.connect(pipelayer , SIGNAL("layerDeleted ()")    , self.disconnectPipeLayer )
-				self.pipelayer = pipelayer
-				print "qWat: connecting SIGNAL for pipes (layer: %s)" % pipelayer.name()
-
 	def PipeSearchDlg(self):
 		dlg = PipeSearch(self.pipelayer, self.iface )
 		if self.pipelayer == 0:
