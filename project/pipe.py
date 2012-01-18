9b076a3bf4a61b0e5d98dc4c6deaee775c6843f1
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-01-18 16:42:33 +0100

    ui: update pipe form

--- project/pipe.py
+++ project/pipe.py
@@ -1,20 +1,10 @@
 """
-/***************************************************************************
- qWat - pipe init form script
-                              -------------------
-        begin                : 2011-12-21
-        copyright            : (C) 2011 by Denis Rouzaud
-        email                : denis.rouzaud@sige.ch
- ***************************************************************************/
+Triangulation QGIS plugin
+Denis Rouzaud
+denis.rouzaud@gmail.com
+Jan. 2012
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+qWat - qgis water module
 """
 
 # -*- coding: utf-8 -*-
