40b7e6366907ff6a21677b3b93f31468a86ae698
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-01-18 09:57:23 +0100

    removed personnal stuff from comments

--- qWat/__init__.py
+++ qWat/__init__.py
@@ -31,22 +31,3 @@ def classFactory(iface):
     # load Sige class from file Sige
     from qwat import qWat
     return qWat(iface)
-    
-"""  
-pyrcc4 -o /home/denis/Documents/qWat/qWat/resources.py /home/denis/Documents/qWat/qWat/resources.qrc
-  
-pyuic4 -o /home/denis/Documents/qWat/qWat/ui_ItemBrowser.py /home/denis/Documents/qWat/qWat/ui_ItemBrowser.ui 
-pyuic4 -o /home/denis/Documents/qWat/qWat/ui_PipeSearch.py /home/denis/Documents/qWat/qWat/ui_PipeSearch.ui 
-pyuic4 -o /home/denis/Documents/qWat/qWat/ui_ConnectLayers.py /home/denis/Documents/qWat/qWat/ui_ConnectLayers.ui 
-
-pylupdate4             /home/denis/Documents/qWat/qWat/*.py /home/denis/Documents/qWat/qWat/*.ui -ts qWat_fr.ts
-pylupdate4 -noobsolete /home/denis/Documents/qWat/qWat/*.py /home/denis/Documents/qWat/qWat/*.ui -ts qWat_fr.ts
-
-lrelease /home/denis/Documents/qWat/qWat/qWat_fr.ts -qm /home/denis/Documents/qWat/qWat/qWat.qm
-
-rm /home/denis/.qgis/python/plugins/qWat/*; cp /home/denis/Documents/qWat/qWat/* /home/denis/.qgis/python/plugins/qWat/; qgis /home/denis/Documents/qWat/project/distribution_v19.qgs &
-"""
-
-
-
-
