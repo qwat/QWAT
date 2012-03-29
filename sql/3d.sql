7a7bb3213ae5ea63b12036067f8a1c300fe4ab2d
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-03-29 15:31:22 +0200

    SQL: completed *d length calculation

--- sql/3d.sql
+++ sql/3d.sql
@@ -5,7 +5,7 @@
 */
 
 /*
-raster2pgsql DTM distribution.dtm > load_dtm.sql
+raster2pgsql mnt.asc distribution.dtm > load_dtm.sql
 psql load_dtm.sql
 */
 
@@ -16,16 +16,32 @@ CREATE OR REPLACE FUNCTION distribution.length3d(geometry) RETURNS double precis
 		altitude double precision;
 		length double precision;
 		i RECORD;
+		interpolateDistance integer := 4;
+		rasterid integer;
+		intersect boolean;
 	BEGIN
+		/* Interpolate along the line */
+		SELECT ST_Segmentize(geom,interpolateDistance) INTO geom;
+		/* Create third coordinate (Z) */
 		SELECT ST_Force_3DZ(geom) INTO geom;
+		/* Loop along all points */
 		FOR i IN SELECT generate_series(1,ST_NumPoints(geom)) AS i LOOP
+			/* Create 2d point */
 			SELECT ST_Force_2D(ST_PointN( geom , i.i )) INTO point ;
-			SELECT ST_Value(rast, ST_SetSRID( point , 21781 )) FROM distribution.dtm INTO altitude;
+			/* Check that we have the good raster id */
+			IF (SELECT ST_Intersects(rast,1,point) FROM distribution.dtm WHERE rid = rasterid) IS NOT TRUE THEN
+				SELECT distribution.getRasterId(point) INTO rasterid;
+			END IF;
+			/* Get altitude */
+			SELECT ST_Value(rast,point) FROM distribution.dtm WHERE rid= rasterid INTO altitude;
+			/* If we don''t have altitude of a point, return NULL */
 			IF altitude IS NULL THEN 
 				RETURN NULL;
 			END IF;
+			/* Set altitude in 3rd coordinate in line geometry */
 			SELECT ST_SetPoint( geom , i.i-1 , ST_MakePoint(ST_X(point),ST_Y(point),altitude) ) INTO geom;
 		END LOOP;
+		/* Calculates 3d length */
 		SELECT ST_3DLength(geom) INTO length;
 		RETURN length;
 	END
@@ -33,9 +49,17 @@ CREATE OR REPLACE FUNCTION distribution.length3d(geometry) RETURNS double precis
 COMMENT ON FUNCTION distribution.length3d(geometry) IS 'Returns the real (3D) length of a linestring using the dtm (raster) stored in distribution.dtm. To create the DTM use raster2pgsql DTM distribution.dtm > load_dtm.sql and load the resulting file.';
 
 
-/* 
- 
-*/
+CREATE OR REPLACE FUNCTION distribution.getRasterId(geometry) RETURNS integer AS '
+	DECLARE
+		geom ALIAS FOR $1;
+		rasterid integer;
+	BEGIN
+		SELECT rid FROM distribution.dtm WHERE ST_Intersects(ST_SetSRID(rast,21781),1,geom) LIMIT 1 INTO rasterid;
+		RETURN rasterid;
+	END
+' LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION distribution.getRasterId(geometry) IS 'Returns the first raster ID that match geometry location.';
+
 
 CREATE OR REPLACE FUNCTION distribution.pipes_length3d() RETURNS void AS '
 	DECLARE
