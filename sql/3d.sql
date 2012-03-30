0b1ced3781e135d5eb03f5172610fd8889c7ae36
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-03-30 15:03:17 +0200

    SQL: fix calculation of 3d length

--- sql/3d.sql
+++ sql/3d.sql
@@ -66,8 +66,8 @@ CREATE OR REPLACE FUNCTION distribution.pipes_length3d() RETURNS void AS '
 		length double precision;
 		pipeitem RECORD;
 	BEGIN
-		FOR pipeitem IN SELECT id,wkb_geometry FROM distribution.pipes WHERE _length3d_uptodate IS NOT TRUE ORDER BY id LOOP
-			IF pipes.tunnel_or_bridge IS TRUE THEN
+		FOR pipeitem IN SELECT id,wkb_geometry,tunnel_or_bridge FROM distribution.pipes WHERE _length3d_uptodate IS NOT TRUE ORDER BY id LOOP
+			IF pipeitem.tunnel_or_bridge IS TRUE THEN
 				length := pipeitem._length2d;
 			ELSE
 				RAISE NOTICE ''%'', pipeitem.id;
