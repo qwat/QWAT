e6ce41d82ecef673365603821d0847dfe9e5727b
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-04-10 07:31:25 +0200

    SQL: completed installations

--- sql/installations.sql
+++ sql/installations.sql
@@ -13,23 +13,31 @@ ALTER TABLE distribution.installations ADD COLUMN  name       varchar(30) DEFAUL
 ALTER TABLE distribution.installations ADD COLUMN  id_type    integer               ;
 SELECT AddGeometryColumn('distribution', 'installations', 'geometry', 21781, 'POINT', 2);
 SELECT setval('distribution.installations_id_seq', 100, true);
-/* ADD CONSTRAINTS */
 /* primary key */
 ALTER TABLE distribution.installations ADD CONSTRAINT installations_pkey PRIMARY KEY (id);
 /* GIST index*/
 CREATE INDEX installations_geoidx ON distribution.installations USING GIST ( geometry );
 /* Comment */
 COMMENT ON TABLE distribution.installations IS 'installations.';
-
 /*----------------!!!---!!!----------------*/
 /* Add constraints */
-/* primary key */
+/* id_type */
 ALTER TABLE distribution.installations ADD CONSTRAINT installations_id_type FOREIGN KEY (id_type) REFERENCES distribution.installations_type(id) MATCH FULL ON UPDATE NO ACTION ON DELETE NO ACTION;
 CREATE INDEX fki_installations_id_type ON distribution.installations(id_type);
 
 
 
-
-
+/* VIEW */
+DROP VIEW IF EXISTS distribution.installations_view ;
+CREATE VIEW distribution.installations_view AS 
+	SELECT
+		installations.id                                ,
+		installations.name                              ,
+		installations.id_type                           ,
+		installations.geometry::geometry(Point,21781)   ,
+		installations_type.name       AS _type_name     ,
+		installations_type.short_name AS _type_shortname
+	FROM distribution.installations 
+	INNER JOIN distribution.installations_type ON installations.id_type = installations_type.id ;
 COMMIT;
 
--- sql/installations_type.sql
+++ sql/installations_type.sql
@@ -10,26 +10,26 @@ CREATE TABLE distribution.installations_type ( id SERIAL, CONSTRAINT "installati
 
 /* Columns*/
 ALTER TABLE distribution.installations_type ADD COLUMN "short_name" VARCHAR(5);
-ALTER TABLE distribution.installations_type ADD COLUMN "type"       VARCHAR(30);
+ALTER TABLE distribution.installations_type ADD COLUMN "name"       VARCHAR(30);
 
 /* Constraints*/
-ALTER TABLE distribution.installations_type ADD CONSTRAINT type_name UNIQUE ("type");
+ALTER TABLE distribution.installations_type ADD CONSTRAINT type_name UNIQUE ("name");
 
 /* Comment */
 COMMENT ON TABLE distribution.installations_type IS 'type for installations.';
 
 /* Predefined types list */
 /* This has to be filled by user to fullfile language preferences */
-INSERT INTO distribution.installations_type (short_name,type) VALUES ('I'  ,'Inconnu');
-INSERT INTO distribution.installations_type (short_name,type) VALUES ('R'  ,'Réservoir');
-INSERT INTO distribution.installations_type (short_name,type) VALUES ('S'  ,'Source');
-INSERT INTO distribution.installations_type (short_name,type) VALUES ('P'  ,'Pompage');
-INSERT INTO distribution.installations_type (short_name,type) VALUES ('CV' ,'Chambres de vannes');
-INSERT INTO distribution.installations_type (short_name,type) VALUES ('CC' ,'Chambre compteur');
-INSERT INTO distribution.installations_type (short_name,type) VALUES ('CT' ,'Chambre de traitement');
-INSERT INTO distribution.installations_type (short_name,type) VALUES ('CR' ,'Chambre réducteur');
-INSERT INTO distribution.installations_type (short_name,type) VALUES ('CCP','Chambre coupe pression');
-INSERT INTO distribution.installations_type (short_name,type) VALUES ('CRA','Chambre de rassemblement');
+INSERT INTO distribution.installations_type (short_name,name) VALUES ('I'  ,'Inconnu');
+INSERT INTO distribution.installations_type (short_name,name) VALUES ('R'  ,'Réservoir');
+INSERT INTO distribution.installations_type (short_name,name) VALUES ('S'  ,'Source');
+INSERT INTO distribution.installations_type (short_name,name) VALUES ('P'  ,'Pompage');
+INSERT INTO distribution.installations_type (short_name,name) VALUES ('CV' ,'Chambres de vannes');
+INSERT INTO distribution.installations_type (short_name,name) VALUES ('CC' ,'Chambre compteur');
+INSERT INTO distribution.installations_type (short_name,name) VALUES ('CT' ,'Chambre de traitement');
+INSERT INTO distribution.installations_type (short_name,name) VALUES ('CR' ,'Chambre réducteur');
+INSERT INTO distribution.installations_type (short_name,name) VALUES ('CCP','Chambre coupe pression');
+INSERT INTO distribution.installations_type (short_name,name) VALUES ('CRA','Chambre de rassemblement');
 
 
 COMMIT;
