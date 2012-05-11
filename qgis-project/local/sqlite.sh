1124fbade898d9849c2bfef2a3429b58dabb3b55
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-05-11 14:06:04 +0200

    completed export

--- qgis-project/local/sqlite.sh
+++ qgis-project/local/sqlite.sh
@@ -6,33 +6,93 @@ export ogr2ogrpath=/usr/local/bin/ogr2ogr
 export ogr2ogrpath=ogr2ogr
 
 
-rm distribution.sqlite
+rm sige_distribution.sqlite
 
 
 # PIPES
-$ogr2ogrpath -sql "SELECT * FROM distribution.pipes_view" \
- -overwrite -a_srs EPSG:21781 -f SQLite distribution.sqlite \
+$ogr2ogrpath -sql "SELECT                                           \
+		id				  ,                                         \
+		year              ,                                         \
+		tunnel_or_bridge  ,                                         \
+		pressure_nominale ,                                         \
+		folder            ,                                         \
+		remarks           ,                                         \
+		_length2d         ,                                         \
+		_length3d         ,                                         \
+		_length3d_uptodate,                                         \
+		_is_on_map        ,                                         \
+		_is_on_district   ,                                         \
+		geometry::geometry(LineString,21781),                       \
+		sqrt(pow(_length3d,2)-pow(_length2d,2))/_length2d AS _slope,\
+		_function_name,                                             \
+		_install_method,                                            \
+		_material_name,                                             \
+		_material_longname,                                         \
+		_material_diameter,                                         \
+		_material_diameter_internal,                                \
+		_owner,                                                     \
+		_precision,                                                 \
+		_protection,                                                \
+		_status_name,                                               \
+		_status_active,                                             \
+		_pressure_zone                                              \
+ FROM distribution.pipes_view WHERE id_owner = 1" \
+ -overwrite -a_srs EPSG:21781 -f SQLite sige_distribution.sqlite \
  -nln pipes -nlt LINESTRING -progress \
  PG:"dbname='sige' host='172.24.171.202' port='5432' user='sige' password='db4wat$'" \
  -dsco SPATIALITE=yes -lco SPATIAL_INDEX=yes  -gt 65536
 
 # PIPES SCHEMA
-$ogr2ogrpath -sql "SELECT * FROM distribution.pipes_schema" \
- -overwrite -a_srs EPSG:21781 -f SQLite distribution.sqlite \
+$ogr2ogrpath -sql "SELECT                                            \
+		id				  ,                                         \
+		year              ,                                         \
+		tunnel_or_bridge  ,                                         \
+		pressure_nominale ,                                         \
+		folder            ,                                         \
+		remarks           ,                                         \
+		_length2d         ,                                         \
+		_length3d         ,                                         \
+		_length3d_uptodate,                                         \
+		_is_on_map        ,                                         \
+		_is_on_district   ,                                         \
+		geometry::geometry(LineString,21781),                       \
+		sqrt(pow(_length3d,2)-pow(_length2d,2))/_length2d AS _slope,\
+		_function_name,                                             \
+		_install_method,                                            \
+		_material_name,                                             \
+		_material_longname,                                         \
+		_material_diameter,                                         \
+		_material_diameter_internal,                                \
+		_owner,                                                     \
+		_precision,                                                 \
+		_protection,                                                \
+		_status_name,                                               \
+		_status_active,                                             \
+		_pressure_zone                                              \
+ FROM distribution.pipes_schema WHERE id_owner = 1" \
+ -overwrite -a_srs EPSG:21781 -f SQLite sige_distribution.sqlite \
  -nln pipes_schema -nlt LINESTRING -progress \
  PG:"dbname='sige' host='172.24.171.202' port='5432' user='sige' password='db4wat$'" \
  -dsco SPATIALITE=yes -lco SPATIAL_INDEX=yes  -gt 65536
 
 # NODES
-$ogr2ogrpath -sql "SELECT * FROM distribution.nodes" \
- -overwrite -a_srs EPSG:21781 -f SQLite distribution.sqlite \
+$ogr2ogrpath -sql "SELECT * FROM distribution.nodes WHERE _status_active IS TRUE" \
+ -overwrite -a_srs EPSG:21781 -f SQLite sige_distribution.sqlite \
  -nln nodes -nlt POINT -progress \
  PG:"dbname='sige' host='172.24.171.202' port='5432' user='sige' password='db4wat$'" \
  -dsco SPATIALITE=yes -lco SPATIAL_INDEX=yes  -gt 65536
  
 # INSTALLATIONS
 $ogr2ogrpath -sql "SELECT * FROM distribution.installations" \
- -overwrite -a_srs EPSG:21781 -f SQLite distribution.sqlite \
+ -overwrite -a_srs EPSG:21781 -f SQLite sige_distribution.sqlite \
  -nln installations -nlt POINT -progress \
+ PG:"dbname='sige' host='172.24.171.202' port='5432' user='sige' password='db4wat$'" \
+ -dsco SPATIALITE=yes -lco SPATIAL_INDEX=yes  -gt 65536
+ 
+ 
+ # PRESSURE ZONES
+$ogr2ogrpath -sql "SELECT * FROM distribution.pressure_zones" \
+ -overwrite -a_srs EPSG:21781 -f SQLite sige_distribution.sqlite \
+ -nln pressure_zones -nlt POINT -progress \
  PG:"dbname='sige' host='172.24.171.202' port='5432' user='sige' password='db4wat$'" \
  -dsco SPATIALITE=yes -lco SPATIAL_INDEX=yes  -gt 65536
