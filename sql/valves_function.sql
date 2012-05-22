ab567ca891303bf6a7fcb26be7e2e351a54f2c63
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-05-22 15:45:36 +0200

    fix column names

--- sql/valves_function.sql
+++ sql/valves_function.sql
@@ -19,17 +19,17 @@ ALTER TABLE distribution.valves_function ADD CONSTRAINT unique_function UNIQUE (
 /* Comment */
 COMMENT ON TABLE distribution.valves_function IS 'Types of valves';
 
-INSERT INTO distribution.valves_function ( function ) VALUES ('vanne de régulation');      /* 1  REG' */
-INSERT INTO distribution.valves_function ( function ) VALUES ('ventouse','Ve');            /* 2  VE   */
-INSERT INTO distribution.valves_function ( function ) VALUES ('vanne by-pass');            /* 3  VBP  */
-INSERT INTO distribution.valves_function ( function ) VALUES ('vanne d''ouvrage','','false');/* 4  OUV  */
-INSERT INTO distribution.valves_function ( function ) VALUES ('organe abonné','','false'); /* 5  OA   */
-INSERT INTO distribution.valves_function ( function ) VALUES ('prise de secours');         /* 6  SEC  */
-INSERT INTO distribution.valves_function ( function ) VALUES ('vanne incendie');           /* 7  VIN  */
-INSERT INTO distribution.valves_function ( function ) VALUES ('vanne d''hydrant');         /* 8  VH   */
-INSERT INTO distribution.valves_function ( function ) VALUES ('inconnu');                  /* 9 I     */
-INSERT INTO distribution.valves_function ( function ) VALUES ('vidange','Vi');             /* 10 VID  */
-INSERT INTO distribution.valves_function ( function ) VALUES ('organe réseau');            /* 11 VR   */
-INSERT INTO distribution.valves_function ( function ) VALUES ('vidange-ventouse','ViVe');  /* 12 VIVE */
+INSERT INTO distribution.valves_function ( function )               VALUES ('vanne de régulation');      /* 1  REG' */
+INSERT INTO distribution.valves_function ( function , shortname )   VALUES ('ventouse','Ve');            /* 2  VE   */
+INSERT INTO distribution.valves_function ( function )               VALUES ('vanne by-pass');            /* 3  VBP  */
+INSERT INTO distribution.valves_function ( function , schema_view ) VALUES ('vanne d''ouvrage','false');/* 4  OUV  */
+INSERT INTO distribution.valves_function ( function , schema_view ) VALUES ('organe abonné','false'); /* 5  OA   */
+INSERT INTO distribution.valves_function ( function )               VALUES ('prise de secours');         /* 6  SEC  */
+INSERT INTO distribution.valves_function ( function )               VALUES ('vanne incendie');           /* 7  VIN  */
+INSERT INTO distribution.valves_function ( function )               VALUES ('vanne d''hydrant');         /* 8  VH   */
+INSERT INTO distribution.valves_function ( function )               VALUES ('inconnu');                  /* 9 I     */
+INSERT INTO distribution.valves_function ( function , shortname )   VALUES ('vidange','Vi');             /* 10 VID  */
+INSERT INTO distribution.valves_function ( function )               VALUES ('organe réseau');            /* 11 VR   */
+INSERT INTO distribution.valves_function ( function , shortname )   VALUES ('vidange-ventouse','ViVe');  /* 12 VIVE */
 
 COMMIT;
