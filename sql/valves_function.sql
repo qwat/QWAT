a10c0517456dd135e277366c180fe6c70df40616
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-05-22 11:27:07 +0200

    added short names2 2

---
+++ sql/valves_function.sql
@@ -0,0 +1,35 @@
+/*
+	qWat - QGIS Water Module
+	
+	SQL file :: valves_function table
+*/
+BEGIN;
+
+DROP TABLE IF EXISTS distribution.valves_function CASCADE;
+CREATE TABLE distribution.valves_function ( id SERIAL, CONSTRAINT "valves_function_pk" PRIMARY KEY (id));                          
+
+/* Columns*/
+ALTER TABLE distribution.valves_function ADD COLUMN "function" VARCHAR(30);
+ALTER TABLE distribution.valves_function ADD COLUMN "short_name" VARCHAR(5);
+ALTER TABLE distribution.valves_function ADD COLUMN "schema_view" BOOLEAN NOT NULL DEFAULT true;
+
+/* Constraints*/
+ALTER TABLE distribution.valves_function ADD CONSTRAINT unique_function UNIQUE ("function");
+
+/* Comment */
+COMMENT ON TABLE distribution.valves_function IS 'Types of valves';
+
+INSERT INTO distribution.valves_function ( function ) VALUES ('vanne de régulation');      /* 1  REG' */
+INSERT INTO distribution.valves_function ( function ) VALUES ('ventouse','Ve');            /* 2  VE   */
+INSERT INTO distribution.valves_function ( function ) VALUES ('vanne by-pass');            /* 3  VBP  */
+INSERT INTO distribution.valves_function ( function ) VALUES ('vanne d''ouvrage');         /* 4  OUV  */
+INSERT INTO distribution.valves_function ( function ) VALUES ('organe abonné','','false'); /* 5  OA   */
+INSERT INTO distribution.valves_function ( function ) VALUES ('prise de secours');         /* 6  SEC  */
+INSERT INTO distribution.valves_function ( function ) VALUES ('vanne incendie');           /* 7  VIN  */
+INSERT INTO distribution.valves_function ( function ) VALUES ('vanne d''hydrant');         /* 8  VH   */
+INSERT INTO distribution.valves_function ( function ) VALUES ('inconnu');                  /* 9 I     */
+INSERT INTO distribution.valves_function ( function ) VALUES ('vidange','Vi');             /* 10 VID  */
+INSERT INTO distribution.valves_function ( function ) VALUES ('organe réseau');            /* 11 VR   */
+INSERT INTO distribution.valves_function ( function ) VALUES ('vidange-ventouse','ViVe');  /* 12 VIVE */
+
+COMMIT;
