7b7c229b8e4300f621b39a0c36340cbc74b4e981
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-05-29 09:45:42 +0200

    added Chatel

--- sql/owner.sql
+++ sql/owner.sql
@@ -27,6 +27,7 @@ INSERT INTO distribution.owner (name) VALUES ('Privé');
 INSERT INTO distribution.owner (name) VALUES ('Blonay');
 INSERT INTO distribution.owner (name) VALUES ('Villeneuve');
 INSERT INTO distribution.owner (name) VALUES ('St-Légier');
+INSERT INTO distribution.owner (name) VALUES ('Châtel');
 
 
 COMMIT;
