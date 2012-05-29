1fcd7a1d3014985e83e433c9788f52aaaa58e491
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-05-29 09:46:38 +0200

    added join with owner

--- sql/installations_view.sql
+++ sql/installations_view.sql
@@ -14,13 +14,15 @@ CREATE VIEW distribution.installations_view AS
 		installations.name                              ,
 		installations.id_type                           ,
 		installations.id_owner                          ,
-		installations.status                          ,
-		installations.remarks                          ,
+		installations.status                            ,
+		installations.remarks                           ,
 		installations.geometry::geometry(Point,21781)   ,
 		installations_type.name       AS _type_name     ,
-		installations_type.short_name AS _type_shortname
+		installations_type.short_name AS _type_shortname,
+		owner.name AS _owner
 	FROM distribution.installations 
-	INNER JOIN distribution.installations_type ON installations.id_type = installations_type.id ;
+	INNER      JOIN distribution.installations_type ON installations.id_type  = installations_type.id 
+	LEFT OUTER JOIN distribution.owner              ON installations.id_owner = owner.id ;
 
 
 /* TODO: rules */
