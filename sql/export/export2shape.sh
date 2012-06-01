e1f8e22c32027cfd7d04b74624af6b7da20fd1fc
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-06-01 11:25:27 +0200

    correct nodes schema and added valves

--- sql/export/export2shape.sh
+++ sql/export/export2shape.sh
@@ -1,18 +1,46 @@
+export db_address=172.24.171.202
+
+
+# vannes: prendre fermée + vanne régulation/secour (fct) 
+
+# save schema in a table
+#psql -h $db_address -U sige -c "DROP TABLE IF EXISTS distribution.pipes_schema_temp; CREATE TABLE distribution.pipes_schema_temp AS SELECT * FROM distribution.pipes_schema_nodes;"
+
+ # vannes
+/usr/bin/pgsql2shp -h $db_address -g geom -f /home/denis/Documents/PPDE/out/vannes -P db4wat$ -u sige sige "\
+SELECT \
+	id ,                                        \
+	sige,                                       \
+	id_pipe,                                    \
+	id_node,                                    \
+	diameter_nominal,                           \
+	year,                                       \
+	closed,                                     \
+	altitude_dtm,                               \
+	altitude_real,                              \
+	remarks,                                    \
+	_is_on_map,                                 \
+	_is_on_district,                            \
+	geometry::geometry(Point,21781) AS geom,    \
+	_function,                                  \
+	_type,                                      \
+	_label                                      \
+ FROM distribution.valves_schema WHERE closed IS TRUE OR _function = 'vanne de régulation' " 
 
 # zones
-/usr/bin/pgsql2shp -h 172.24.171.202 -g geom -f /home/denis/Documents/PPDE/out/zones -P db4wat$ -u sige sige "\
+/usr/bin/pgsql2shp -h $db_address -g geom -f /home/denis/Documents/PPDE/out/zones -P db4wat$ -u sige sige "\
 SELECT	                                                                       \
 	id AS ID,                                                                  \
 	name AS NOM,                                                               \
-	geometry AS geom                                                           \
+	geometry::geometry(Polygon,21781) AS geom                                                           \
 FROM distribution.pressure_zones"
 
 
 # conduites
-/usr/bin/pgsql2shp -h 172.24.171.202 -g geom -f /home/denis/Documents/PPDE/out/conduites -P db4wat$ -u sige sige "\
+/usr/bin/pgsql2shp -h $db_address -g geom -f /home/denis/Documents/PPDE/out/conduites -P db4wat$ -u sige sige "\
 SELECT                                                                             \
 	pipes_schema.id AS ID,                                                         \
-	geometry AS geom,                                                              \
+	geometry::geometry(LineString,21781) AS geom,                                                              \
 	_length2d AS LONGU_2D,                                                         \
 	CASE WHEN _length3d_uptodate IS TRUE                                           \
 		THEN _length3d                                                             \
@@ -31,28 +59,28 @@ SELECT                                                                          
 	NULL::varchar(10) AS RUGOSITE,                                                 \
 	NULL::boolean AS CALC_HYD,                                                     \
 	NULL::boolean AS A_DESAFE                                                      \
-FROM distribution.pipes_schema WHERE id_owner = 1 "
+FROM distribution.pipes_schema_nodes WHERE id_owner = 1 "
 
 # ouvrages
-/usr/bin/pgsql2shp -h 172.24.171.202 -g geom -f /home/denis/Documents/PPDE/out/ouvrages -P db4wat$ -u sige sige "\
+/usr/bin/pgsql2shp -h $db_address -g geom -f /home/denis/Documents/PPDE/out/ouvrages -P db4wat$ -u sige sige "\
 SELECT                                                                            \
 	id AS ID,                                                                     \
-	geometry AS geom,                                                             \
+	geometry::geometry(Point,21781) AS geom,                                                             \
 	name AS NOM,                                                                  \
 	_type_name AS TYPE                                                            \
 FROM distribution.installations_view"
 
 # noeuds
-/usr/bin/pgsql2shp -h 172.24.171.202 -g geom -f /home/denis/Documents/PPDE/out/noeuds -P db4wat$ -u sige sige "\
+/usr/bin/pgsql2shp -h $db_address -g geom -f /home/denis/Documents/PPDE/out/noeuds -P db4wat$ -u sige sige "\
 SELECT                                                                          \
 	id AS ID,                                                                   \
 	altitude_dtm AS ALTITUDE,                                                   \
-	geometry AS geom                                                            \
+	geometry::geometry(Point,21781) AS geom                                                            \
 FROM distribution.nodes                                                         \
 WHERE _schema_view IS TRUE                                                      \
   AND _status_Active IS TRUE                                                    \
   AND id IN (                                                                   \
 	SELECT DISTINCT(id_node_a)                                                  \
-	FROM distribution.pipes_schema WHERE id_owner = 1                           \
+	FROM distribution.pipes_schema_temp WHERE id_owner = 1                           \
 	UNION SELECT DISTINCT(id_node_b)                                            \
-	FROM distribution.pipes_schema WHERE id_owner = 1 )"
+	FROM distribution.pipes_schema_temp WHERE id_owner = 1 )"
