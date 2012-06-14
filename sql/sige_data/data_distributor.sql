1ba15d8f38402e7736b82be60ccec1948fde062d
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-06-14 11:50:59 +0200

    owner => distributor

---
+++ sql/sige_data/data_distributor.sql
@@ -0,0 +1,16 @@
+BEGIN;
+INSERT INTO distribution.distributor (name) VALUES ('SIGE');			      /* 1   */
+INSERT INTO distribution.distributor (name) VALUES ('ES Lausanne');          /*  2   */
+INSERT INTO distribution.distributor (name) VALUES ('Privé');                /*   3  */
+INSERT INTO distribution.distributor (name) VALUES ('Blonay');               /*   4  */
+INSERT INTO distribution.distributor (name) VALUES ('Villeneuve');           /*   5  */
+INSERT INTO distribution.distributor (name) VALUES ('St-Légier');            /*   6  */
+INSERT INTO distribution.distributor (name) VALUES ('Châtel');               /*   7  */
+INSERT INTO distribution.distributor (name) VALUES ('Romande Energie');      /*  8   */
+INSERT INTO distribution.distributor (name) VALUES ('Caux initiative et Changement');   /*   9  */
+INSERT INTO distribution.distributor (name) VALUES ('Puidoux');              /*   10  */
+INSERT INTO distribution.distributor (name) VALUES ('MOB');                  /*   11  */
+INSERT INTO distribution.distributor (name) VALUES ('Réseau des Planches');  /*   12  */
+INSERT INTO distribution.distributor (name) VALUES ('Réseau de Remaufens');  /*   13  */
+INSERT INTO distribution.distributor (name) VALUES ('Réseau de Noville');    /*   14  */
+COMMIT;
