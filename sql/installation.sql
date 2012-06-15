fbc0fd74cc0f3f5e131e66ba1e549129745484a7
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-06-15 08:48:28 +0200

    installations => installation

--- sql/installation.sql
+++ sql/installation.sql
@@ -1,38 +1,38 @@
 /*
 	qWat - QGIS Water Module
 	
-	SQL file :: installations
+	SQL file :: installation
 */
 
 BEGIN;
 
 /* CREATE TABLE */
-DROP TABLE IF EXISTS distribution.installations CASCADE;
-CREATE TABLE distribution.installations (id serial NOT NULL);
-SELECT setval('distribution.installations_id_seq', 100, true);
-COMMENT ON TABLE distribution.installations IS 'installations.';
+DROP TABLE IF EXISTS distribution.installation CASCADE;
+CREATE TABLE distribution.installation (id serial NOT NULL);
+SELECT setval('distribution.installation_id_seq', 100, true);
+COMMENT ON TABLE distribution.installation IS 'installation.';
 
 /* columns */
-ALTER TABLE distribution.installations ADD COLUMN  name            varchar(30) DEFAULT '';
-ALTER TABLE distribution.installations ADD COLUMN  id_type         integer                ;
-ALTER TABLE distribution.installations ADD COLUMN  id_distributor  integer                ;
-ALTER TABLE distribution.installations ADD COLUMN  id_status       integer                ;
-ALTER TABLE distribution.installations ADD COLUMN  remarks         text                    ;
-ALTER TABLE distribution.installations ADD COLUMN  schema_force_view  boolean DEFAULT NULL::boolean;
+ALTER TABLE distribution.installation ADD COLUMN  name            varchar(30) DEFAULT '';
+ALTER TABLE distribution.installation ADD COLUMN  id_type         integer                ;
+ALTER TABLE distribution.installation ADD COLUMN  id_distributor  integer                ;
+ALTER TABLE distribution.installation ADD COLUMN  id_status       integer                ;
+ALTER TABLE distribution.installation ADD COLUMN  remarks         text                    ;
+ALTER TABLE distribution.installation ADD COLUMN  schema_force_view  boolean DEFAULT NULL::boolean;
 
 /* geometry */
-SELECT AddGeometryColumn('distribution', 'installations', 'geometry', 21781, 'POINT', 2);
-CREATE INDEX installations_geoidx ON distribution.installations USING GIST ( geometry );
+SELECT AddGeometryColumn('distribution', 'installation', 'geometry', 21781, 'POINT', 2);
+CREATE INDEX installation_geoidx ON distribution.installation USING GIST ( geometry );
 
 /* primary key */
-ALTER TABLE distribution.installations ADD CONSTRAINT installations_pkey PRIMARY KEY (id);
+ALTER TABLE distribution.installation ADD CONSTRAINT installation_pkey PRIMARY KEY (id);
 
 
 /* Constraints */
 /* id_type */
-ALTER TABLE distribution.installations ADD CONSTRAINT installations_id_type        FOREIGN KEY (id_type)        REFERENCES distribution.installations_type(id)   MATCH FULL; CREATE INDEX fki_installations_id_type        ON distribution.installations(id_type)        ;
-ALTER TABLE distribution.installations ADD CONSTRAINT installations_id_distributor FOREIGN KEY (id_distributor) REFERENCES distribution.distributor(id)          MATCH FULL; CREATE INDEX fki_installations_id_distributor ON distribution.installations(id_distributor) ;
-ALTER TABLE distribution.installations ADD CONSTRAINT installations_id_status      FOREIGN KEY (id_status)      REFERENCES distribution.status(id)               MATCH FULL; CREATE INDEX fki_installations_id_status      ON distribution.installations(id_status)      ;
+ALTER TABLE distribution.installation ADD CONSTRAINT installation_id_type        FOREIGN KEY (id_type)        REFERENCES distribution.installation_type(id)   MATCH FULL; CREATE INDEX fki_installation_id_type        ON distribution.installation(id_type)        ;
+ALTER TABLE distribution.installation ADD CONSTRAINT installation_id_distributor FOREIGN KEY (id_distributor) REFERENCES distribution.distributor(id)          MATCH FULL; CREATE INDEX fki_installation_id_distributor ON distribution.installation(id_distributor) ;
+ALTER TABLE distribution.installation ADD CONSTRAINT installation_id_status      FOREIGN KEY (id_status)      REFERENCES distribution.status(id)               MATCH FULL; CREATE INDEX fki_installation_id_status      ON distribution.installation(id_status)      ;
 
 
 
--- sql/installation_view.sql
+++ sql/installation_view.sql
@@ -1,35 +1,35 @@
 /*
 	qWat - QGIS Water Module
 	
-	SQL file :: installations view
+	SQL file :: installation view
 */
 
 BEGIN;
 
 
-DROP VIEW IF EXISTS distribution.installations_view ;
-CREATE VIEW distribution.installations_view AS 
+DROP VIEW IF EXISTS distribution.installation_view ;
+CREATE VIEW distribution.installation_view AS 
 	SELECT
-		installations.id                                ,
-		installations.name                              ,
-		installations.id_type                           ,
-		installations.id_distributor                    ,
-		installations.id_status                         ,
-		installations.remarks                           ,
-		installations.geometry::geometry(Point,21781)   ,
-		installations_type.name       AS _type          ,
-		installations_type.short_name AS _type_shortname,
+		installation.id                                ,
+		installation.name                              ,
+		installation.id_type                           ,
+		installation.id_distributor                    ,
+		installation.id_status                         ,
+		installation.remarks                           ,
+		installation.geometry::geometry(Point,21781)   ,
+		installation_type.name       AS _type          ,
+		installation_type.short_name AS _type_shortname,
 		status.status                 AS _status        ,
 		status.active                 AS _status_active ,
 		distributor.name              AS _distributor   ,
 		CASE 
-			WHEN installations.schema_force_view IS NULL THEN installations_type.schema_view
-			ELSE installations.schema_force_view
+			WHEN installation.schema_force_view IS NULL THEN installation_type.schema_view
+			ELSE installation.schema_force_view
 		END AS _schema_view
-	FROM distribution.installations 
-	INNER      JOIN distribution.installations_type ON installations.id_type        = installations_type.id 
-	INNER      JOIN distribution.status             ON installations.id_status      = status.id 
-	LEFT OUTER JOIN distribution.distributor        ON installations.id_distributor = distributor.id ;
+	FROM distribution.installation 
+	INNER      JOIN distribution.installation_type ON installation.id_type        = installation_type.id 
+	INNER      JOIN distribution.status             ON installation.id_status      = status.id 
+	LEFT OUTER JOIN distribution.distributor        ON installation.id_distributor = distributor.id ;
 
 
 /* TODO: rules */
