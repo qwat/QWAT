a166cc244dab4f8fbaac3832919778a0edf797ac
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-06-25 08:21:27 +0200

    created view to forward color code

--- sql/samplingpoint.sql
+++ sql/samplingpoint.sql
@@ -10,9 +10,42 @@ DROP TABLE IF EXISTS distribution.samplingpoint CASCADE;
 CREATE TABLE distribution.samplingpoint (id serial NOT NULL);
 SELECT setval('distribution.samplingpoint_id_seq', 200, true);
 
+/* columns */
+ALTER TABLE distribution.samplingpoint ADD COLUMN  id_district      integer       ;
+ALTER TABLE distribution.samplingpoint ADD COLUMN  id_pressurezone  integer       ;
+ALTER TABLE distribution.samplingpoint ADD COLUMN  remarks          text          ;
+ALTER TABLE distribution.samplingpoint ADD COLUMN  _is_on_map       varchar(30)   ;
+
 /* geometry */
 SELECT AddGeometryColumn('distribution', 'samplingpoint', 'geometry', 21781, 'POINT', 2);                                                                                        
 CREATE INDEX samplingpoint_geoidx ON distribution.samplingpoint USING GIST ( geometry );
+
+
+/* Trigger for map and district update */
+CREATE OR REPLACE FUNCTION distribution.samplingpoint_geom() RETURNS trigger AS ' 
+	BEGIN
+		UPDATE distribution.samplingpoint SET 
+			id_pressurezone    = distribution.get_pressurezone_id(NEW.geometry),
+			id_district        = distribution.get_district_id(NEW.geometry),
+			_is_on_map         = distribution.get_map(NEW.geometry)
+		WHERE id = NEW.id ;
+		RETURN NEW;
+	END;
+' LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION distribution.samplingpoint_geom() IS 'Fcn/Trigger: updates auto fields of the samplingpoint after insert/update.';
+
+CREATE TRIGGER samplingpoint_geom_trigger_insert
+	AFTER INSERT ON distribution.samplingpoint
+	FOR EACH ROW
+	EXECUTE PROCEDURE distribution.samplingpoint_geom();
+COMMENT ON TRIGGER samplingpoint_geom_trigger_insert ON distribution.samplingpoint IS 'Trigger: updates auto fields of the samplingpoint after insert.';
+
+CREATE TRIGGER samplingpoint_geom_trigger_update
+	AFTER UPDATE OF geometry ON distribution.samplingpoint 
+	FOR EACH ROW
+	WHEN (ST_AsBinary(NEW.geometry) <> ST_AsBinary(OLD.geometry))
+	EXECUTE PROCEDURE distribution.samplingpoint_geom();
+COMMENT ON TRIGGER samplingpoint_geom_trigger_update ON distribution.samplingpoint IS 'Trigger: updates auto fields of the samplingpoint after geom update.';
 
 
 COMMIT; 
---
+++ sql/samplingpoint_view.sql
@@ -0,0 +1,51 @@
+/*
+	qWat - QGIS Water Module
+	
+	SQL file :: samplingpoint view
+*/
+
+BEGIN;
+
+DROP VIEW IF EXISTS distribution.samplingpoint_view CASCADE;
+CREATE VIEW distribution.samplingpoint_view AS 
+	SELECT  
+		samplingpoint.id             ,
+		samplingpoint.id_district    ,
+		samplingpoint.id_pressurezone,
+		samplingpoint._is_on_map     ,
+		samplingpoint.remarks        ,
+		samplingpoint.geometry::geometry(Point,21781),
+		district.name              AS _district,
+		pressurezone.name          AS _pressurezone,
+		pressurezone.shortname     AS _pressurezone_shortname,
+		pressurezone.consummerzone AS _consummerzone,
+		pressurezone.colorcode     AS _pressurezone_colorcode
+		FROM distribution.samplingpoint
+		LEFT OUTER JOIN distribution.district      ON samplingpoint.id_district     = district.id       
+		LEFT OUTER JOIN  distribution.pressurezone ON samplingpoint.id_pressurezone = pressurezone.id;
+/*----------------!!!---!!!----------------*/
+/* Comment */
+COMMENT ON VIEW distribution.samplingpoint_view IS 'View for samplingpoint. This view is editable (a rule exists to forwad changes to the table).';
+
+/*----------------!!!---!!!----------------*/
+/* INSERT,UPDATE,DELETE RULES */
+CREATE OR REPLACE RULE samplingpoint_update AS
+	ON UPDATE TO distribution.samplingpoint_view DO INSTEAD
+		UPDATE distribution.samplingpoint SET 
+			remarks         = NEW.remarks 
+		WHERE id = NEW.id;
+CREATE OR REPLACE RULE samplingpoint_insert AS
+	ON INSERT TO distribution.samplingpoint_view DO INSTEAD
+		INSERT INTO distribution.samplingpoint 
+			(    remarks,    geometry)     
+		VALUES
+			(NEW.remarks,NEW.geometry);
+CREATE OR REPLACE RULE samplingpoint_delete AS
+	ON DELETE TO distribution.samplingpoint_view DO INSTEAD
+		DELETE FROM distribution.samplingpoint WHERE id = OLD.id;
+/* Comments */	
+COMMENT ON RULE samplingpoint_update IS 'Rule to forward changes for samplingpoint_view to the table samplingpoint.';
+COMMENT ON RULE samplingpoint_insert IS 'Rule to forward insert of samplingpoint_view to the table samplingpoint.';
+COMMENT ON RULE samplingpoint_delete IS 'Rule to forward deletion of samplingpoint_view to the table samplingpoint.';
+
+COMMIT;
