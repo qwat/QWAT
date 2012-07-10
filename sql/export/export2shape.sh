c543a73adab6138e39c8c1de30fcd717980b041a
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-07-10 18:58:01 +0200

    PDDE export

--- sql/export/export2shape.sh
+++ sql/export/export2shape.sh
@@ -5,7 +5,7 @@ export shapeoutput=/home/denis/Documents/PPDE/out
 # vannes: prendre fermée + vanne régulation/secour (fct) 
 
 # save schema in a table
-#psql -h $db_address -U sige -c "DROP TABLE IF EXISTS distribution.pipe_schema_temp; CREATE TABLE distribution.pipe_schema_temp AS SELECT * FROM distribution.pipe_schema_node;"
+psql -h $db_address -U sige -c "DROP TABLE IF EXISTS distribution.pipe_schema_temp; CREATE TABLE distribution.pipe_schema_temp AS SELECT * FROM distribution.pipe_schema_node;"
 read -p "Press any key to continue..."
 
  # vannes 
@@ -45,45 +45,45 @@ read -p "Press any key to continue..."
 
 # conduites
 pgsql2shp -h $db_address -g geom -f $shapeoutput/conduites -P db4wat$ -u sige sige "\
-SELECT                                          \
- id AS ID,                         \
+SELECT                                         \
+ id                          AS ID,            \
  geometry::geometry(LineString,21781) AS geom, \
- _length2d AS LONGU_2D,                        \
+ _length2d                   AS LONGU_2D,      \
  CASE WHEN _length3d_uptodate IS TRUE          \
   THEN _length3d                               \
   ELSE NULL                                    \
- END AS LONGU_3D,                              \
- remarks AS REMARQUE,                          \
- _precision AS PRECISIO,                       \
- _distributor AS DISTRIBU,                     \
- _function_name AS FONCTION,                   \
- _material_longname AS MATERIAU,               \
+ END                         AS LONGU_3D,      \
+ remarks                     AS REMARQUE,      \
+ _precision                  AS PRECISIO,      \
+ _distributor                AS DISTRIBU,      \
+ _function_name              AS FONCTION,      \
+ _material_longname          AS MATERIAU,      \
  _material_diameter_internal AS DIAM_INT,      \
- _pressurezone AS ZONE_PRES,                   \
- _valve_closed AS VAN_FERM,                    \
- year AS ANNEE,                                \
- id_node_a AS NOEUD_A,                         \
- id_node_b AS NOEUD_B,                         \
- NULL::varchar(10) AS PRESSION,                \
- NULL::varchar(10) AS RUGOSITE,                \
- NULL::boolean AS CALC_HYD,                    \
- NULL::boolean AS A_DESAFE                     \
+ _pressurezone               AS ZONE_PRES,     \
+ _valve_closed               AS VAN_FERM,      \
+ year                        AS ANNEE,         \
+ id_node_a                   AS NOEUD_A,       \
+ id_node_b                   AS NOEUD_B,       \
+ NULL::varchar(10)           AS PRESSION,      \
+ NULL::varchar(10)           AS RUGOSITE,      \
+ NULL::boolean               AS CALC_HYD,      \
+ NULL::boolean               AS A_DESAFE       \
 FROM distribution.pipe_schema_temp WHERE id_distributor = 1 "
 read -p "Press any key to continue..."
 
 # ouvrages
 pgsql2shp -h $db_address -g geom -f $shapeoutput/ouvrages -P db4wat$ -u sige sige "\
 SELECT                                      \
- id AS ID,                                  \
+ id      AS ID,                             \
  id_node AS NOEUD,                          \
  geometry::geometry(Point,21781) AS geom,   \
- name AS NOM,                               \
- _type AS TYPE,                             \
+ name    AS NOM,                            \
+ _type   AS TYPE,                           \
  CASE                                       \
     WHEN altitude_real IS NOT NULL THEN altitude_real \
     ELSE _altitude_dtm                      \
- END AS ALTITUDE,                           \
- _district AS COMMUNNE,                     \
+ END           AS ALTITUDE,                 \
+ _district     AS COMMUNNE,                 \
  _pressurezone AS ZONE_PRES                 \
 FROM distribution.installation_view"        
 read -p "Press any key to continue..."
@@ -91,7 +91,7 @@ read -p "Press any key to continue..."
 # noeuds
 pgsql2shp -h $db_address -g geom -f $shapeoutput/noeuds -P db4wat$ -u sige sige "\
 SELECT                                                           \
- id AS NOEUD,                                                       \
+ id AS NOEUD,                                                    \
  altitude_dtm AS ALTITUDE,                                       \
  geometry::geometry(Point,21781) AS geom                         \
 FROM distribution.node                                           \
@@ -123,16 +123,16 @@ SELECT                                    \
  model          AS MODELE,                \
  remarks        AS REMARQUE,              \
  geometry::geometry(Point,21781) AS geom, \
- _type AS TYPE,                           \
- _distributor AS FOURNISS,                \
- _status_name AS STATUT,                  \
+ _type          AS TYPE,                  \
+ _distributor   AS FOURNISS,              \
+ _status_name   AS STATUT,                \
  _status_active AS ACTIF,                 \
- _provider AS DISTRIB,                    \
+ _provider      AS DISTRIB,               \
  CASE                                     \
     WHEN altitude_real IS NOT NULL THEN altitude_real \
     ELSE _altitude_dtm                    \
- END AS ALTITUDE,                         \
- _district AS COMMUNNE,                   \
- _pressurezone AS ZONE_PRES               \
+ END            AS ALTITUDE,              \
+ _district      AS COMMUNNE,              \
+ _pressurezone  AS ZONE_PRES              \
 FROM distribution.hydrant_view "
 read -p "Press any key to continue..."
