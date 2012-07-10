2e11be8216caa617bbd2edd64609a7b1354aa5a0
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-07-10 15:51:37 +0200

    join with nodes and pressure zones

--- sql/valve_schema.sql
+++ sql/valve_schema.sql
@@ -21,7 +21,6 @@ CREATE VIEW distribution.valve_schema AS
 		valve.diameter_nominal,
 		valve.year,
 		valve.closed,
-		valve.altitude_dtm,
 		valve.altitude_real,
 		valve.remarks,
 		valve.schema_force_view,
@@ -30,15 +29,22 @@ CREATE VIEW distribution.valve_schema AS
 		valve_function.function  AS _function,
 		valve_function.shortname AS _function_shortname,
 		valve_type.type          AS _type,
-		district.name            AS _district,
 		CASE 
 			WHEN valve_function.shortname IS NULL THEN valve.sige::varchar
 			ELSE valve_function.shortname || valve.sige::varchar
-		END AS _label
+		END AS _label,
+		node.altitude_dtm          AS _altitude_dtm,
+		district.name              AS _district,
+		pressurezone.name          AS _pressurezone,
+		pressurezone.shortname     AS _pressurezone_shortname,
+		pressurezone.consummerzone AS _consummerzone,
+		pressurezone.colorcode     AS _pressurezone_colorcode
 		FROM distribution.valve
 		INNER JOIN distribution.valve_type     ON valve.id_function = valve_type.id
 		INNER JOIN distribution.valve_function ON valve.id_function = valve_function.id
-		LEFT OUTER JOIN distribution.district  ON valve.id_district = district.id 
+		LEFT OUTER JOIN distribution.node           ON valve.id_node         = node.id       
+		LEFT OUTER JOIN distribution.district       ON valve.id_district     = district.id       
+		LEFT OUTER JOIN distribution.pressurezone   ON valve.id_pressurezone = pressurezone.id
 		WHERE  valve.schema_force_view IS TRUE 
 		   OR (valve.schema_force_view IS NULL AND valve_function.schema_view IS TRUE);
 		
