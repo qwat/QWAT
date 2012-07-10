91a57ecb52eef9cf2cfc74149cb90eaf093b61bd
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-07-10 11:20:22 +0200

    backup procedure

--- sql/export/backup.bash
+++ sql/export/backup.bash
@@ -10,7 +10,7 @@ read -p "Are you sure to export relations? Previous SQL file will deleted! (y/n)
 if [[ "$answ" == "y" ]]
 then
 	#psql -h 172.24.171.202 -U sige -c "SELECT 'UPDATE distribution.pipe SET id_parent='||id_parent||' WHERE id='||id||';', id, id_parent, ST_AsText(ST_StartPoint(geometry)),ST_AsText(ST_EndPoint(geometry)) FROM distribution.pipe WHERE id_parent IS NOT NULL ORDER BY id_parent;" > /home/denis/Documents/qgis/qwat/sql/sige_data/pipe.parent.dat
-	psql -h 172.24.171.202 -U sige -c "SELECT 'UPDATE distribution.pipe SET id_parent='||id_parent||' WHERE id='||id||';' FROM distribution.pipe WHERE id_parent IS NOT NULL ORDER BY id_parent;" > temp
+	psql -h 172.24.171.202 -U sige -c "SELECT 'UPDATE distribution.pipe SET id_parent='||id_parent||' WHERE id='||id||';' FROM distribution.pipe WHERE id_parent IS NOT NULL ORDER BY id,id_parent;" > temp
 	awk 'match($0,"UPDATE") !=0 {print $0}' temp > temp2
 	sed '1i BEGIN;'  temp2 > temp
 	sed '$a COMMIT;' temp > temp2
