06580b0f00f21734afd4cb5e2ad547cb17108b98
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-07-12 08:14:04 +0200

    used $ in function definition

--- sql/node_fcn.sql
+++ sql/node_fcn.sql
@@ -8,16 +8,19 @@ BEGIN;
 
 
 /* set node altitude */
-CREATE OR REPLACE FUNCTION distribution.node_altitude() RETURNS void AS '
+CREATE OR REPLACE FUNCTION distribution.node_altitude() RETURNS void AS
+$BODY$
 	BEGIN
 		UPDATE distribution.node SET altitude_dtm = altitude.altitude(geometry) WHERE altitude_dtm IS NULL ;
-	END
-' LANGUAGE 'plpgsql';
+	END;
+$BODY$
+LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION distribution.node_altitude() IS 'Fill the altitude of the node.';
 
 
 /* get node id */
-CREATE OR REPLACE FUNCTION distribution.node_get_id(geometry,boolean) RETURNS integer AS '
+CREATE OR REPLACE FUNCTION distribution.node_get_id(geometry,boolean) RETURNS integer AS
+$BODY$
 	DECLARE
 		point ALIAS for $1;
 		place_node ALIAS for $2;
@@ -30,12 +33,14 @@ CREATE OR REPLACE FUNCTION distribution.node_get_id(geometry,boolean) RETURNS in
 		END IF;
 		RETURN node_id;	
 	END;
-' LANGUAGE 'plpgsql';
+$BODY$
+LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION distribution.node_get_id(geometry,boolean) IS 'Returns the node for a given geometry (point). If node does not exist and if specified in argument, it is created.';
 
 /* define node type */
 /* node type: end, intersection, year, material, diameter */
-CREATE OR REPLACE FUNCTION distribution.node_type(integer) RETURNS void AS '
+CREATE OR REPLACE FUNCTION distribution.node_type(integer) RETURNS void AS
+$BODY$
 	DECLARE
 		node_id     ALIAS FOR $1         ;
 		pipeitem    RECORD               ;
@@ -58,13 +63,13 @@ CREATE OR REPLACE FUNCTION distribution.node_type(integer) RETURNS void AS '
 			WHERE (id_node_a = node_id OR id_node_b = node_id);
 		IF grouped.count = 0 THEN
 			IF node_id IN (SELECT id_node FROM distribution.valve) THEN
-				type := ''valve'';
+				type := 'valve';
 			ELSE
-				RAISE NOTICE ''Delete node %'' , node_id ;
+				RAISE NOTICE 'Delete node %' , node_id ;
 				DELETE FROM distribution.node WHERE id = node_id ;
 			END IF;
 		ELSEIF grouped.count = 1 THEN
-			type := ''one'';
+			type := 'one';
 		ELSEIF grouped.count = 2 THEN
 			FOR pipeitem IN (
 				SELECT year,_material_longname,_material_diameter,ST_PointN(geometry,2)                      AS point_1 , ST_StartPoint(geometry) AS point_2 FROM distribution.pipe_view WHERE id_node_a = node_id
@@ -78,16 +83,16 @@ CREATE OR REPLACE FUNCTION distribution.node_type(integer) RETURNS void AS '
 					i := 1;
 					SELECT  ST_Azimuth(pipeitem.point_1,pipeitem.point_2) INTO ori1 ;
 				ELSE
-					type := ''two_same'';
-					IF Tyear     != pipeitem.year               THEN type := ''two_year''    ; END IF;
-					IF Tmaterial != pipeitem._material_longname THEN type := ''two_material''; END IF;
-					IF Tdiameter != pipeitem._material_diameter THEN type := ''two_diameter''; END IF;
+					type := 'two_same';
+					IF Tyear     != pipeitem.year               THEN type := 'two_year'    ; END IF;
+					IF Tmaterial != pipeitem._material_longname THEN type := 'two_material'; END IF;
+					IF Tdiameter != pipeitem._material_diameter THEN type := 'two_diameter'; END IF;
 					SELECT ST_Azimuth(pipeitem.point_1,pipeitem.point_2) INTO ori2 ;
 					SELECT degrees( ATAN2( (SIN(ori1)+SIN(ori2))/2 , (COS(ori1)+COS(ori2))/2 ) ) INTO orientation;
 				END IF;
 			END LOOP;
 		ELSEIF grouped.count > 2 THEN
-			type := ''three'';
+			type := 'three';
 		END IF;
 		UPDATE distribution.node SET 
 			_type          = type,
@@ -95,13 +100,15 @@ CREATE OR REPLACE FUNCTION distribution.node_type(integer) RETURNS void AS '
 			_schema_view   = grouped.schema_view,
 			_status_active = grouped.status_active
 			WHERE id = node_id;
-		/*RAISE NOTICE ''% %'' , node_id , orientation;*/
+		/*RAISE NOTICE '% %' , node_id , orientation;*/
 	END;
-' LANGUAGE 'plpgsql';
+$BODY$
+LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION distribution.node_type(integer) IS 'Set the orientation and type for a node. If three pipe arrives at the node: intersection. If one pipe: end. If two: depends on characteristics of pipe: year (is different), material (and year), diameter(and material/year)';
 
 /* reset all node type */
-CREATE OR REPLACE FUNCTION distribution.node_set_type() RETURNS void AS '
+CREATE OR REPLACE FUNCTION distribution.node_set_type() RETURNS void AS
+$BODY$
 	DECLARE
 		node record;
 	BEGIN
@@ -109,7 +116,8 @@ CREATE OR REPLACE FUNCTION distribution.node_set_type() RETURNS void AS '
 			PERFORM distribution.node_type(node.id);
 		END LOOP;
 	END;
-' LANGUAGE 'plpgsql';
+$BODY$
+LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION distribution.node_set_type() IS 'Set the type and orientation for node. If three pipe arrives at the node: intersection. If one pipe: end. If two: depends on characteristics of pipe: year (is different), material (and year), diameter(and material/year)';
 
   
