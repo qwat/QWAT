31423015ab83ec76f3ca96ab28cbbf1848ed977e
Denis Rouzaud <denis.rouzaud@gmail.com>
2013-02-14 08:22:50 +0100

    sepraration SQL pour structure et donnees

---
+++ sql/load.bash
@@ -0,0 +1,21 @@
+
+./sql.bash
+
+# SIGE DATA
+read -p "Press any key to continue..."
+psql -h 172.24.171.$ip -U sige -f sige_data/data_district.sql -v ON_ERROR_STOP=1
+read -p "Press any key to continue..."
+psql -h 172.24.171.$ip -U sige -f sige_data/data_distributor.sql -v ON_ERROR_STOP=1
+read -p "Press any key to continue..."
+psql -h 172.24.171.$ip -U sige -f sige_data/data_pressurezone.sql -v ON_ERROR_STOP=1
+read -p "Press any key to continue..."
+psql -h 172.24.171.$ip -U sige -f sige_data/data_node.sql -v ON_ERROR_STOP=1
+read -p "Press any key to continue..."
+psql -h 172.24.171.$ip -U sige -f sige_data/data_samplingpoint.sql -v ON_ERROR_STOP=1
+read -p "Press any key to continue..."
+psql -h 172.24.171.$ip -U sige -f sige_data/data_protectionzone.sql -v ON_ERROR_STOP=1
+
+
+# TODO AFTER PIPE IMPORT
+read -p "Import SQL fini -> proceder a l'import FME -> puis continuer..."
+psql -h 172.24.171.$ip -U sige -f sige_data/data_pipe_id_parent.sql -v ON_ERROR_STOP=1
--- sql/sql.bash
+++ sql/sql.bash
@@ -125,25 +125,5 @@ psql -h 172.24.171.$ip -U sige -f search_view.sql
 read -p "Press any key to continue..."
 psql -h 172.24.171.$ip -U sige -f control/control_node.sql
 
-
-# SIGE DATA
-read -p "Press any key to continue..."
-psql -h 172.24.171.$ip -U sige -f sige_data/data_district.sql -v ON_ERROR_STOP=1
-read -p "Press any key to continue..."
-psql -h 172.24.171.$ip -U sige -f sige_data/data_distributor.sql -v ON_ERROR_STOP=1
-read -p "Press any key to continue..."
-psql -h 172.24.171.$ip -U sige -f sige_data/data_pressurezone.sql -v ON_ERROR_STOP=1
-read -p "Press any key to continue..."
-psql -h 172.24.171.$ip -U sige -f sige_data/data_node.sql -v ON_ERROR_STOP=1
-read -p "Press any key to continue..."
-psql -h 172.24.171.$ip -U sige -f sige_data/data_samplingpoint.sql -v ON_ERROR_STOP=1
-read -p "Press any key to continue..."
-psql -h 172.24.171.$ip -U sige -f sige_data/data_protectionzone.sql -v ON_ERROR_STOP=1
-
-
-# TODO AFTER PIPE IMPORT
-read -p "Press any key to continue..."
-psql -h 172.24.171.$ip -U sige -f sige_data/data_pipe_id_parent.sql -v ON_ERROR_STOP=1
-
 fi
 
