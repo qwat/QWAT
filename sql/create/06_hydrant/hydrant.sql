16b1466346e74b7e52b95094af3c88063f92b3d3
Denis Rouzaud <denis.rouzaud@gmail.com>
2013-10-25 09:38:53 +0200

    watering outputs separated from hydrants, completed hydrants

--- sql/create/06_hydrant/hydrant.sql
+++ sql/create/06_hydrant/hydrant.sql
@@ -13,27 +13,20 @@ SELECT setval('distribution.hydrant_id_seq', 100, true);
 COMMENT ON TABLE distribution.hydrant IS 'hydrant.';
 
 /* columns */
-ALTER TABLE distribution.hydrant ADD COLUMN  identification  character varying (15)  ;
-ALTER TABLE distribution.hydrant ADD COLUMN  id_type          integer                 ;
-ALTER TABLE distribution.hydrant ADD COLUMN  id_distributor   integer                 ;
-ALTER TABLE distribution.hydrant ADD COLUMN  id_status        integer                 ;
-ALTER TABLE distribution.hydrant ADD COLUMN  id_provider      integer                 ;
-ALTER TABLE distribution.hydrant ADD COLUMN  year smallint    CHECK (year > 1800 AND year < 2100);
-ALTER TABLE distribution.hydrant ADD COLUMN  model            varchar(30)             ;
-ALTER TABLE distribution.hydrant ADD COLUMN  altitude_real    decimal(10,3)           ;
-ALTER TABLE distribution.hydrant ADD COLUMN  remarks text;
+ALTER TABLE distribution.hydrant ADD COLUMN identification  character varying (15)   ;
+ALTER TABLE distribution.hydrant ADD COLUMN id_distributor   integer                 ;
+ALTER TABLE distribution.hydrant ADD COLUMN id_status        integer                 ;
+ALTER TABLE distribution.hydrant ADD COLUMN id_provider      integer                 ;
+ALTER TABLE distribution.hydrant ADD COLUMN id_precision     integer                 ;
+ALTER TABLE distribution.hydrant ADD COLUMN year smallint    CHECK (year > 1800 AND year < 2100);
+ALTER TABLE distribution.hydrant ADD COLUMN model            varchar(30)             ;
+ALTER TABLE distribution.hydrant ADD COLUMN underground      boolean DEFAULT false   ;
+ALTER TABLE distribution.hydrant ADD COLUMN altitude_real    decimal(10,3)           ;
+ALTER TABLE distribution.hydrant ADD COLUMN remarks text;
 
-/*
-partie inferieure
-sortie centrale checkbox
-plaque montee cb
-emplacement
-precis alti
-statut
-troncons
-*/
 
-/* geometry */
+
+/* geometry                        (table_name, is_node, create_node, create_schematic, get_pipe, auto_district)*/
 SELECT distribution.geom_tool_point('hydrant',true,true,true,false,true);
 
 /* primary key */
@@ -41,10 +34,9 @@ ALTER TABLE distribution.hydrant ADD CONSTRAINT hydrant_pkey PRIMARY KEY (id);
 
 
 /* Constraints */
-/* id_type */
-ALTER TABLE distribution.hydrant ADD CONSTRAINT hydrant_id_type         FOREIGN KEY (id_type)         REFERENCES distribution.hydrant_type(id)     MATCH FULL  ; CREATE INDEX fki_hydrant_id_type         ON distribution.hydrant(id_type)        ;
 ALTER TABLE distribution.hydrant ADD CONSTRAINT hydrant_id_distributor  FOREIGN KEY (id_distributor)  REFERENCES distribution.distributor(id)      MATCH FULL  ; CREATE INDEX fki_hydrant_id_distributor  ON distribution.hydrant(id_distributor) ;
 ALTER TABLE distribution.hydrant ADD CONSTRAINT hydrant_id_status       FOREIGN KEY (id_status)       REFERENCES distribution.status(id)           MATCH FULL  ; CREATE INDEX fki_hydrant_id_status       ON distribution.hydrant(id_status)      ;
 ALTER TABLE distribution.hydrant ADD CONSTRAINT hydrant_id_provider     FOREIGN KEY (id_provider)     REFERENCES distribution.hydrant_provider(id) MATCH SIMPLE; CREATE INDEX fki_hydrant_id_provider     ON distribution.hydrant(id_provider)    ;
+ALTER TABLE distribution.hydrant ADD CONSTRAINT hydrant_id_precision    FOREIGN KEY (id_precision)    REFERENCES distribution.precision(id)        MATCH FULL  ; CREATE INDEX fki_hydrant_id_precision    ON distribution.hydrant(id_precision)   ;
 
 
---
+++ sql/create/06_hydrant/wateringoutput.sql
@@ -0,0 +1,35 @@
+/*
+	qWat - QGIS Water Module
+	
+	SQL file :: wateringoutput
+*/
+
+
+
+/* CREATE TABLE */
+DROP TABLE IF EXISTS distribution.wateringoutput CASCADE;
+CREATE TABLE distribution.wateringoutput (id serial NOT NULL);
+SELECT setval('distribution.wateringoutput_id_seq', 100, true);
+COMMENT ON TABLE distribution.wateringoutput IS 'wateringoutput.';
+
+/* columns */
+ALTER TABLE distribution.wateringoutput ADD COLUMN  identification  character varying (15)  ;
+ALTER TABLE distribution.wateringoutput ADD COLUMN  id_distributor   integer                 ;
+ALTER TABLE distribution.wateringoutput ADD COLUMN  id_status        integer                 ;
+ALTER TABLE distribution.wateringoutput ADD COLUMN  year smallint    CHECK (year > 1800 AND year < 2100);
+ALTER TABLE distribution.wateringoutput ADD COLUMN  altitude_real    decimal(10,3)           ;
+ALTER TABLE distribution.wateringoutput ADD COLUMN  remarks text;
+
+/* geometry                        (table_name, is_node, create_node, create_schematic, get_pipe, auto_district)*/
+SELECT distribution.geom_tool_point('wateringoutput',  true,    true,        false,            false,    true);
+
+/* primary key */
+ALTER TABLE distribution.wateringoutput ADD CONSTRAINT wateringoutput_pkey PRIMARY KEY (id);
+
+
+/* Constraints */
+/* id_type */
+ALTER TABLE distribution.wateringoutput ADD CONSTRAINT wateringoutput_id_distributor  FOREIGN KEY (id_distributor)  REFERENCES distribution.distributor(id)      MATCH FULL  ; CREATE INDEX fki_wateringoutput_id_distributor  ON distribution.wateringoutput(id_distributor) ;
+ALTER TABLE distribution.wateringoutput ADD CONSTRAINT wateringoutput_id_status       FOREIGN KEY (id_status)       REFERENCES distribution.status(id)           MATCH FULL  ; CREATE INDEX fki_wateringoutput_id_status       ON distribution.wateringoutput(id_status)      ;
+
+
