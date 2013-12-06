81adf65ba7d34902eb21a93e81c124745600e991
Denis Rouzaud <denis.rouzaud@gmail.com>
2013-12-06 10:41:46 +0100

    remove diagram

--- sql/diagram/generate.sh
+++
@@ -1,7 +0,0 @@
-export schema=distribution
-export db=sige
-
-postgresql_autodoc -d $db -f $schema -h 172.24.171.203 -u $db -s $schema
-
-dot -Tpng -o $schema.png $schema.dot
-dot -Tsvg -o $schema.svg $schema.dot
