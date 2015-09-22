566abb3fcc99e621f716139c8bdd4a5e8266b4ff
Denis Rouzaud <denis.rouzaud@gmail.com>
2015-09-22 10:35:02 +0200

    follow up installation new strucutre
    * field definition
    * form
    * symbology

--- qgis-project/python/installation_form.py
+++ qgis-project/python/installation_form.py
@@ -1,14 +1,22 @@
 
 from PyQt4 import uic
 from PyQt4.QtGui import QComboBox, QWidget
-from qgis.core import QgsProject
 
 def formOpen(dialog, layer, feature):
-	
+
 	typeSelector = dialog.findChild(QComboBox, 'installation_type')
 	insType = typeSelector.property('EWV2Wrapper').value()
-	
-	childWidgetContent = uic.loadUi("%s/ui_forms/%s.ui" % (QgsProject.instance().readPath("./"), insType))
-	childWidget = dialog.findChild(QWidget, 'childWidget')
-	
-	childWidget.layout().addWidget(childWidgetContent)
+
+	subForms = ['chamber',
+	            'pressurecontrol',
+	            'pump',
+	            'source',
+	            'tank',
+	            'treatment']
+
+	subForms.remove(insType)
+
+	for sf in subForms:
+		childWidget = dialog.findChild(QWidget, sf)
+		childWidget.hide()
+
