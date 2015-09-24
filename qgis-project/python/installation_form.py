49785ed37d618049c94878be405e820cf64a2b16
Denis Rouzaud <denis.rouzaud@gmail.com>
2015-09-24 10:47:05 +0200

    fix installation form again

--- qgis-project/python/installation_form.py
+++ qgis-project/python/installation_form.py
@@ -7,26 +7,18 @@ subForms = None
 def formOpen(dialog, layer, feature):
 
 	typeSelector = dialog.findChild(QComboBox, 'installation_type')
-	
 	typeSelector.currentIndexChanged.connect(typeSelection)
-	
-	insType = typeSelector.property('EWV2Wrapper').value()
-
-	subFormNames = ['chamber',
-					'pressurecontrol',
-					'pump',
-					'source',
-					'tank',
-					'treatment']
 
 	global subForms
-	subForms = []	
-	for sfn in subFormNames: 
-		childWidget = dialog.findChild(QWidget, sfn)
+	subForms = []
+
+	for i in range(0, typeSelector.count()):
+		widgetName = typeSelector.itemData(i)
+		childWidget = dialog.findChild(QWidget, widgetName)
 		childWidget.hide()
 		subForms.append(childWidget)
-		
-					
-def typeSelection( index ):        
+
+
+def typeSelection( index ):
 	for i, widget in enumerate(subForms):
 		widget.setVisible( i == index )
