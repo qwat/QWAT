16071afefd6749c9be2d06f85339cb5897c0fa1a
Denis Rouzaud <denis.rouzaud@gmail.com>
2015-09-24 09:11:25 +0200

    fix installation form

--- qgis-project/python/installation_form.py
+++ qgis-project/python/installation_form.py
@@ -2,21 +2,31 @@
 from PyQt4 import uic
 from PyQt4.QtGui import QComboBox, QWidget
 
+subForms = None
+
 def formOpen(dialog, layer, feature):
 
 	typeSelector = dialog.findChild(QComboBox, 'installation_type')
+	
+	typeSelector.currentIndexChanged.connect(typeSelection)
+	
 	insType = typeSelector.property('EWV2Wrapper').value()
 
-	subForms = ['chamber',
-	            'pressurecontrol',
-	            'pump',
-	            'source',
-	            'tank',
-	            'treatment']
+	subFormNames = ['chamber',
+					'pressurecontrol',
+					'pump',
+					'source',
+					'tank',
+					'treatment']
 
-	for sf in subForms:
-		if sf == insType:
-			continue
-		childWidget = dialog.findChild(QWidget, sf)
+	global subForms
+	subForms = []	
+	for sfn in subFormNames: 
+		childWidget = dialog.findChild(QWidget, sfn)
 		childWidget.hide()
-
+		subForms.append(childWidget)
+		
+					
+def typeSelection( index ):        
+	for i, widget in enumerate(subForms):
+		widget.setVisible( i == index )
