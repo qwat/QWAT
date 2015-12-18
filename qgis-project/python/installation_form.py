7096d708c611f6c8c4b40766f9699cd8419f60c1
Ludovic Delauné <ludovic.delaune@oslandia.com>
2015-12-18 15:40:43 +0100

    remove useless global statement

--- qgis-project/python/installation_form.py
+++ qgis-project/python/installation_form.py
@@ -1,15 +1,12 @@
 from PyQt4.QtGui import QComboBox, QWidget
 
-subForms = None
+subForms = []
 
 
 def formOpen(dialog, layer, feature):
 
     typeSelector = dialog.findChild(QComboBox, 'installation_type')
     typeSelector.currentIndexChanged.connect(typeSelection)
-
-    global subForms
-    subForms = []
 
     for i in range(0, typeSelector.count()):
         widgetName = typeSelector.itemData(i)
